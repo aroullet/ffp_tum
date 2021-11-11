7dc94a72595ab6384ae228898e0996baa04e4b7f
ga78huf <ga78huf>
2021-11-11 14:45:45 +0100

    added Healthstate, modified cmake version, added attribute healthstate in Person

---
+++ include/HealthState.hpp
@@ -0,0 +1,16 @@
+//
+// Created by fujitsu on 11.11.21.
+//
+
+#ifndef FFP_TUM_HEALTHSTATE_HPP
+#define FFP_TUM_HEALTHSTATE_HPP
+
+
+enum class HealthState {
+    SUSCEPTIBLE,
+    RECOVERED,
+    INFECTED
+};
+
+
+#endif //FFP_TUM_HEALTHSTATE_HPP
--- include/Person.hpp
+++ include/Person.hpp
@@ -1,9 +1,11 @@
 #ifndef FFP_TUM_PERSON_H
 #define FFP_TUM_PERSON_H
+#include "HealthState.hpp"
 
 
 class Person {
-
+private:
+    HealthState healthState = HealthState::SUSCEPTIBLE;
 };
 
 
