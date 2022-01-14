177c72a126b831e86f84b767a1f60969c0b65198
Antonin Roullet <roullet.antonin@gmail.com>
2022-01-14 18:32:31 +0100

    Input check and sanitization now performed in main.cpp. Handles wrong data types as well as integer overflows.

---
+++ include/main.hpp
@@ -0,0 +1,8 @@
+#ifndef FFP_TUM_MAIN_HPP
+#define FFP_TUM_MAIN_HPP
+
+#include <string>
+
+void getUserInput(unsigned int&, const std::string&);
+
+#endif //FFP_TUM_MAIN_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <limits>
 #include "Model.hpp"
 #include "GUI.hpp"
+#include "main.hpp"
+
+void clearInputBuffer() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+};
 
 int main() {
 
@@ -25,14 +31,16 @@ int main() {
     unsigned int height = 500;
     float spreadProb =0.01;//irrelevant so far
     float squareRadius = 100;//nearly direct hits
+
     // Get user input for all variables to initialize model
-    std::cout << "Enter total number of people to visualize: ";
-    std::cin >> nPeople;
-    std::cout << "Enter the number of infected people: ";
-    std::cin >> nInfected;
+    const std::string nPeopleMessage = "Enter total number of people to visualize: ";
+    getUserInput(nPeople, nPeopleMessage);
+    const std::string nInfectedMessage = "Enter the number of infected people: ";
+    getUserInput(nInfected, nInfectedMessage);
+
     while(nInfected>nPeople){
         std::cout << "Number of infected people must be smaller than total number of "<<nPeople<<"! Please enter valid number: ";
-        std::cin >> nInfected;
+        getUserInput(nInfected, "");
     }
 
     GUI gui(nPeople, nInfected, width, height, spreadProb, squareRadius);
@@ -45,4 +53,19 @@ int main() {
     }
 
     return 0;
+}
+
+void getUserInput(unsigned int& value, const std::string& promptMsg) {
+    while (true) {
+        std::cout << promptMsg;
+        std::cin >> value;
+        if (std::cin.fail()) {
+            std::cin.clear();  // reset out of failure mode (also covers over-/underflows)
+            clearInputBuffer(); // remove incorrect input
+            std::cout << "Invalid value specified.\n";
+        } else {
+            clearInputBuffer();
+            break;
+        }
+    }
 }
